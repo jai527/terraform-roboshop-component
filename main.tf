898c0816b3576db3e7fde6e7ca4b04cd8db437d8
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-23 18:54:42 +0530

    all

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ connection {
 }
 
 resource "aws_ami_from_instance" "main_ami" {
-  name               = "${var.project}-${var.environment}-${var.component}"
+  name               = "${var.project}-${var.environment}-${var.component}-ami-"
   source_instance_id = aws_instance.main.id
 
   depends_on = [aws_ec2_instance_state.main]
